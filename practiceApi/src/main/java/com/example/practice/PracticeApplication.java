113602dc87b7643c179de9f48dfbded1faa77a57
Ankur Sahoo <shanky442@gmail.com>
2020-07-14 23:07:28 +0100

    war file generation for gcp app engine

--- practiceApi/src/main/java/com/example/practice/PracticeApplication.java
+++ practiceApi/src/main/java/com/example/practice/PracticeApplication.java
@@ -3,10 +3,11 @@ package com.example.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ComponentScan("com.example")
 @SpringBootApplication
-public class PracticeApplication {
+public class PracticeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
